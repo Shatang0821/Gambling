0f78e272dca3ac502dafb52bfb8ef2cec59c90b4
sato <shatang821@gmail.com>
2024-12-12 14:51:03 +0900

    Update/HealthComponent ロジック記入だけ

--- Gambling/Assets/_Script/Game/Modules/Components/HealthComponent.cs
+++ Gambling/Assets/_Script/Game/Modules/Components/HealthComponent.cs
@@ -1,9 +1,32 @@
 ﻿using FrameWork.Component;
+using Framework.Entity;
 
 namespace Game.Component
 {
     public class HealthComponent : ComponentBase
     {
-        
+        // Data data Entityが持つデータ
+
+        public void TakeDamage(float amount)
+        {
+            if(amount <= 0) return;
+            //if(無敵) return
+            
+            // ダメージ処理
+            // UIイベントをトリガー
+            
+            //HPが0以下
+            //Die();
+        }
+
+        public void Heal(float amount)
+        {
+            if(amount <= 0)return;
+        }
+
+        public void Die()
+        {
+            // イベントをトリガー
+        }
     }
 }
