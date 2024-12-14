c8efdd9cf603a1d3573c35e9351b64d195478a01
sato <shatang821@gmail.com>
2024-12-14 11:00:57 +0900

    Add/Resources Add/EntityData

--- Gambling/Assets/_Script/Game/Modules/Entity/EntityData.cs
+++ Gambling/Assets/_Script/Game/Modules/Entity/EntityData.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "EntityData",menuName = "Data/Entity")]
 public class EntityData : ScriptableObject
+{
+    
+}
+
+[Serializable]
+public class SkillData
 {
     
 }
