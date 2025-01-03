eca9bdfad67724996f67adc4dd0bc855f37c134c
shatang821@gmail.com <shatang821@gmail.com>
2025-01-03 17:21:47 +0900

    Test/Effect

--- Gambling/Assets/_Script/Game/EffectController.cs
+++ Gambling/Assets/_Script/Game/EffectController.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EffectController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator animator;
+
+    private void Awake()
     {
-        
+        // 获取 Animator 组件
+        animator = GetComponent<Animator>();
+        if (animator == null)
+        {
+            Debug.LogWarning("Animator component not found on EffectController.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 激活效果
+    /// </summary>
+    private void OnEnable()
     {
-        
+        // 随机调整 Z 轴旋转
+        float randomZRotation = Random.Range(0f, 360f);
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, randomZRotation);
+
+        // 播放动画
+        if (animator != null)
+        {
+            animator.Play(0, -1, 0); // 播放第一个动画从头开始
+        }
+    }
+
+    /// <summary>
+    /// 可选：自动禁用对象（例如效果完成后）
+    /// </summary>
+    /// <param name="delay">延迟禁用的时间</param>
+    public void DeactivateAfter(float delay)
+    {
+        Invoke(nameof(Deactivate), delay);
+    }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
     }
 }
