dcbeccd3d3cf283b96ecd174fb7d1bcdec1f9071
saitoshintaro <shintarosaito0617@gmail.com>
2025-01-17 09:19:17 +0900

    UpDate/MoveAction

--- Gambling/Assets/_Script/Game/SkillSystem/SkillActions/MoveAction.cs
+++ Gambling/Assets/_Script/Game/SkillSystem/SkillActions/MoveAction.cs
@@ -20,11 +20,9 @@ namespace Game.SkillSystem.Actions
         {
             Debug.Log("Enter Move");
             var movement = owner.GetEntityComponent<MovementComponent>();
-            var collider = owner.GetComponent<BoxCollider2D>();
             if (movement != null)
             {
                 movement.Move(direction: new Vector2(owner.Direction,0),  speed: SkillActionData.Value);
-                collider.enabled = false;
             }
         }
 
@@ -36,11 +34,9 @@ namespace Game.SkillSystem.Actions
         public void Exit(EntityObject owner)
         {
             var movement = owner.GetEntityComponent<MovementComponent>();
-            var collider = owner.GetComponent<BoxCollider2D>();
             if (movement != null)
             {
                 movement.Stop();
-                collider.enabled = true;
                 //Debug.Log("停止");
             }
         }
