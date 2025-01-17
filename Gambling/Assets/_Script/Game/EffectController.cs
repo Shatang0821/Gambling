ce46431f5861c98dfb2eb89662d2715eb02f634d
sato <shatang821@gmail.com>
2025-01-17 09:05:14 +0900

    Update/effect

--- Gambling/Assets/_Script/Game/EffectController.cs
+++ Gambling/Assets/_Script/Game/EffectController.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class EffectController : MonoBehaviour
 {
+    public bool CanRotation = true;
     private Animator animator;
 
     private void Awake()
     {
-        // 获取 Animator 组件
+        // アニメータを取得
         animator = GetComponent<Animator>();
         if (animator == null)
         {
@@ -15,25 +16,28 @@ public class EffectController : MonoBehaviour
     }
 
     /// <summary>
-    /// 激活效果
+    /// 
     /// </summary>
     private void OnEnable()
     {
-        // 随机调整 Z 轴旋转
-        float randomZRotation = Random.Range(0f, 360f);
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, randomZRotation);
-
-        // 播放动画
+        // 回転値をランダムに取る
+        if (CanRotation)
+        {
+            float randomZRotation = Random.Range(0f, 360f);
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, randomZRotation);
+        }
+        
+        // アニメーションを再生
         if (animator != null)
         {
-            animator.Play(0, -1, 0); // 播放第一个动画从头开始
+            animator.Play(0, -1, 0); // 最初から再生
         }
     }
 
     /// <summary>
-    /// 可选：自动禁用对象（例如效果完成后）
+    /// 
     /// </summary>
-    /// <param name="delay">延迟禁用的时间</param>
+    /// <param name="delay"></param>
     public void DeactivateAfter(float delay)
     {
         Invoke(nameof(Deactivate), delay);
